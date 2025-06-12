28a11e730380803b269f1ed3c627c1a111868863
Aurus Soares <neiven131@gmail.com>
2025-06-11 22:28:56 -0300

    feat: Adicionar método GET para lista todos os contatos

--- projetointegrador2/src/main/java/com/puc/go/projetointegrador2/contato/Contato.java
+++ projetointegrador2/src/main/java/com/puc/go/projetointegrador2/contato/Contato.java
@@ -34,4 +34,8 @@ public class Contato {
         this.telefone = telefone;
         this.email = email;
     }
+
+    public Contato(){
+        
+    }
 }
--- projetointegrador2/src/main/java/com/puc/go/projetointegrador2/controller/AgendaEletronicaController.java
+++ projetointegrador2/src/main/java/com/puc/go/projetointegrador2/controller/AgendaEletronicaController.java
@@ -2,9 +2,13 @@ package com.puc.go.projetointegrador2.controller;
 
 import com.puc.go.projetointegrador2.contato.Contato;
 import com.puc.go.projetointegrador2.contato.ContatoRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +25,10 @@ public class AgendaEletronicaController {
     public ResponseEntity<Contato> criarContato(@RequestBody Contato contato) {
         Contato novoContato = contatoRepository.save(contato);
         return ResponseEntity.status(HttpStatus.CREATED).body(novoContato);
+    }
+
+    @GetMapping("/contatos")    
+    public List<Contato> listarTodosContatos(){
+        return contatoRepository.findAll();
     }
 }
