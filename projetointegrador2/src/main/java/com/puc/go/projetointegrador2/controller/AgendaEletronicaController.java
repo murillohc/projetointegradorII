f9e7d29d8d539a098ac39c8d4b27166ffb3bd4fd
Beatriz <bcastroresende@gmail.com>
2025-06-21 10:35:57 -0300

    criando método remover contato

--- projetointegrador2/src/main/java/com/puc/go/projetointegrador2/controller/AgendaEletronicaController.java
+++ projetointegrador2/src/main/java/com/puc/go/projetointegrador2/controller/AgendaEletronicaController.java
@@ -2,12 +2,11 @@ package com.puc.go.projetointegrador2.controller;
 
 import com.puc.go.projetointegrador2.contato.Contato;
 import com.puc.go.projetointegrador2.contato.ContatoRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("agendaEletronica")
@@ -22,14 +21,24 @@ public class AgendaEletronicaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(novoContato);
     }
 
-    @GetMapping("/contato/{name}")
-    public ResponseEntity<Contato> buscarPorNome(@PathVariable String name) {
-        Optional<Contato> contato = Optional.ofNullable(contatoRepository.findByName(name));
+    @GetMapping("/contato/{nome}")
+    public ResponseEntity<Contato> buscarPorNome(@PathVariable String nome) {
+        Optional<Contato> contato = Optional.ofNullable(contatoRepository.findByNome(nome));
 
         if (contato.isPresent()) {
             return ResponseEntity.ok(contato.get());
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> remover(@PathVariable("id") Long id){
+        Optional<Contato> contato = contatoRepository.findById(id);
+        if(contato.isPresent()){
+            contatoRepository.delete(contato.get());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
